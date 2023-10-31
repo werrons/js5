25ec4ac593d8bb147c3257e5fa0ac20df4fa50f9
werrons <bakytsanzirov13@gmail.com>
2023-11-01 00:57:28 +0600

    cikl

---
+++ OneDrive/Рабочий стол/js5/script.js
@@ -0,0 +1,45 @@
+// H O M E   W O R K
+
+//1
+// let a = [1, 2, 3, 0, 4, 5, 6]
+// console.log(a.map(el => el * el))
+
+//2
+// let a = [1, 2, 3, 0, 4, 5, 6]
+// console.log(a.map(el => el >0 ))
+
+//3
+// let a = [1, 2, -3, 0, 4, -5, 6]
+// console.log(a.filter(el => el >0 ))
+
+//4
+// let a = [1, 2, -3, 0, 4, 5, -6];
+// let b = a.filter(el => el < 0);
+// console.log(b);
+
+//5
+// let a = [1, -2, 3, 0, 4, -5, 6]
+// console.log (a.filter(el => el %2 === 0))
+
+
+//6
+// let a = ['hello', 'sit', 'ipsum', 'javascript', 'python', 'sun', 'even'];
+//
+// let b  = a.filter(function(arr) {
+//     return arr.length > 5;
+// });
+
+// console.log(b);
+
+// //FreezBuzz
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 3 === 0 && i %  5 === 0) {
+//         console.log('FizzBuzz');
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(i);
+//     }
+// }
